5137871ecb05189205fd99cb9428cb6a7968d455
parinovK <parinovk@yandex.ru>
2024-03-19 23:26:44 +0300

    init

---
+++ src/main/java/ru/parinov/testSpringBootWithDB/TestSpringBootWithDbApplication.java
@@ -0,0 +1,13 @@
+package ru.parinov.testSpringBootWithDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootWithDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/parinov/testSpringBootWithDB/TestSpringBootWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.parinov.testSpringBootWithDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootWithDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
